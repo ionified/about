da20d78fa411b1d428180fe51549b1eac3a1c7c3
Mike Lee <git@iskitz.com>
2018-01-07 13:31:55 -0800

    todo: added initial entry

---
+++ todo.js
@@ -0,0 +1,17 @@
+;
+~
+{ by: "mike.lee@iskitz"
+, at: "2018.01.07-08"
+, todo
+:   { "2018.01.07-08"
+    :   { "ions.ionified.net"
+        :   { 1: "create subdomain"
+            , 2: `redirect ion paths:
+                  ions.ionified.net/ion.name => github.com/ionified/ion.name-ions.ionified.net
+                  ions.  iskitz.net/ion.name => github.com/ionified/ion.name-ions.isktz.net
+                 `
+            }
+        }
+    }
+}
+;
