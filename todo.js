91af18304ae86fa8b878557efef57ebb582cf145
Mike Lee <git@iskitz.com>
2018-01-07 17:28:11 -0800

    todo: tidied + ion@domain|ip redirect proposal

--- todo.js
+++ todo.js
@@ -1,16 +1,21 @@
 ;
 ~
-{ by: "mike.lee@iskitz"
-, at: "2018.01.07-08"
+{ re:
+    { id: "todos@ionified.net"
+    , by: "mike.lee@iskitz"
+    , at: "2018.01.07-08...2016.11.15-08"
+    , is: "ionified's list of things to do"
+    }
+    
 , todo
 :   { "2018.01.07-08"
     :   { "ions.ionified.net"
-        :   { 1: "create subdomain"
-            , 2: `redirect ion paths:
-                  ions.ionified.net/ion.name => github.com/ionified/ion.name-ions.ionified.net
-                  ions.  iskitz.net/ion.name => github.com/ionified/ion.name-ions.isktz.net
-                 `
-            }
+        :   [ "create subdomain"
+            , `redirect ion paths:
+                  ions.ionified.net/ion.name      => github.com/ionified/ion.name-ions.ionified.net
+                  ions.ionified.net/ion@domain|ip => github.com/ionified/ion.name-domain|ip
+              `
+            ]
         }
     }
 }
