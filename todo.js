4428415d0b661e6a054ac83efe02dccdac01b238
iskitz <git@iskitz.com>
2020-11-04 14:15:11 -0800

    docs: todo: updated to most recent ~re standard & comma-first style

--- todo.js
+++ todo.js
@@ -1,16 +1,19 @@
 ;
 ~
 { re:
-    { id: "todos@ionified.net"
-    , by: "mike.lee@iskitz"
-    , at: "2018.01.07-08...2016.11.15-08"
-    , is: "ionified's list of things to do"
-    }
-    
-, todo
-:   { "2018.01.07-08"
-    :   { "ions.ionified.net"
-        :   [ "create subdomain"
+    { id: 'todos@ionified.net'
+    , is: 'todo'
+    , by: 'mike.lee@iskitz'
+    , on: -8.20161115
+    , to: -8.20201104
+    , at: +0.02
+    , it: "lists ionified's things to do"
+    },
+
+  todo:
+    { '-8.20180107':
+        { "ions.ionified.net":
+            [ "create subdomain"
             , `redirect ion paths:
                   ions.ionified.net/ion.name      => github.com/ionified/ion.name-ions.ionified.net
                   ions.ionified.net/ion@domain|ip => github.com/ionified/ion.name-domain|ip
